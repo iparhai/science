065a44edf41e0c5060bb7bd16136e6481283b0b2
iparhai <iparhai@nu.edu.pk>
2022-05-13 15:14:24 +0500

    Updates

---
+++ precache-manifest.449b143a163df7f9f54c5eaeaf49749a.js
@@ -0,0 +1,554 @@
+self.__precacheManifest = [
+  {
+    "revision": "b557883521ceba541e54",
+    "url": "/science/static/css/main.5cf24a12.chunk.css"
+  },
+  {
+    "revision": "b557883521ceba541e54",
+    "url": "/science/static/js/main.8fb9fa74.chunk.js"
+  },
+  {
+    "revision": "c6a0a3fb19e6fe2cfa71",
+    "url": "/science/static/js/runtime~main.922e013e.js"
+  },
+  {
+    "revision": "9755d617cbc42df2504e",
+    "url": "/science/static/js/2.b1f99c33.chunk.js"
+  },
+  {
+    "revision": "0d6aa5671c2991d389f9db7ebf26151b",
+    "url": "/science/static/media/humanAnatomy.0d6aa567.png"
+  },
+  {
+    "revision": "411c4e6a0a9e83696dc8f57a70f342c0",
+    "url": "/science/static/media/1.411c4e6a.png"
+  },
+  {
+    "revision": "baee8b37557cf9ba7e6c7016c19d3b06",
+    "url": "/science/static/media/10.baee8b37.png"
+  },
+  {
+    "revision": "da95b1d736ed03b78a44d44ef15d813a",
+    "url": "/science/static/media/11.da95b1d7.png"
+  },
+  {
+    "revision": "73fe00995cd7adebd696e8d1715500dc",
+    "url": "/science/static/media/12.73fe0099.png"
+  },
+  {
+    "revision": "5690694919b062e5d201f2895ca093b7",
+    "url": "/science/static/media/13.56906949.png"
+  },
+  {
+    "revision": "7b19bf7bd224cdd9941921ee087b2bdd",
+    "url": "/science/static/media/14.7b19bf7b.png"
+  },
+  {
+    "revision": "2684d075fc2c72e38524ffc4f7d5df41",
+    "url": "/science/static/media/15.2684d075.png"
+  },
+  {
+    "revision": "d1d59a88467a6b381df3eee8ce160f25",
+    "url": "/science/static/media/16.d1d59a88.png"
+  },
+  {
+    "revision": "a7f0e4e3c9b56642615788ea350047d6",
+    "url": "/science/static/media/17.a7f0e4e3.svg"
+  },
+  {
+    "revision": "57a2639bd6d80374aaf0714a48200f92",
+    "url": "/science/static/media/18.57a2639b.svg"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/science/static/media/19.cabc22c6.png"
+  },
+  {
+    "revision": "411c4e6a0a9e83696dc8f57a70f342c0",
+    "url": "/science/static/media/2.411c4e6a.png"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/science/static/media/20.74562e5d.png"
+  },
+  {
+    "revision": "7890a5da3912ba0d306d379bc66c77f3",
+    "url": "/science/static/media/21.7890a5da.png"
+  },
+  {
+    "revision": "5e3851f299527329f4568b35dcd61e82",
+    "url": "/science/static/media/22.5e3851f2.png"
+  },
+  {
+    "revision": "12bf5e12df20a4c25208d42cdd5c800b",
+    "url": "/science/static/media/23.12bf5e12.png"
+  },
+  {
+    "revision": "17b63c151997e22e45c6d3a9fef7305f",
+    "url": "/science/static/media/24.17b63c15.png"
+  },
+  {
+    "revision": "381d702e6ff2a2e0df2c233f2242e658",
+    "url": "/science/static/media/26.381d702e.png"
+  },
+  {
+    "revision": "2de022ec3f3ad9d82c919050ece44677",
+    "url": "/science/static/media/27.2de022ec.png"
+  },
+  {
+    "revision": "1083639a8f108971e623a509c966137a",
+    "url": "/science/static/media/28.1083639a.png"
+  },
+  {
+    "revision": "92bec7dc682a73aa3ea3b776154066c3",
+    "url": "/science/static/media/29.92bec7dc.png"
+  },
+  {
+    "revision": "83e30e91c04bea075e24cb99a2258ec2",
+    "url": "/science/static/media/30.83e30e91.png"
+  },
+  {
+    "revision": "232132e841d3930cf9312824e87e8285",
+    "url": "/science/static/media/31.232132e8.png"
+  },
+  {
+    "revision": "9ea1062648f509a08c76260b83fcc37b",
+    "url": "/science/static/media/32.9ea10626.png"
+  },
+  {
+    "revision": "7d1243f7738aae6551e0df742d95fd82",
+    "url": "/science/static/media/33.7d1243f7.png"
+  },
+  {
+    "revision": "7d5b10d82c061dd0a47d2bfbe43a3280",
+    "url": "/science/static/media/34.7d5b10d8.png"
+  },
+  {
+    "revision": "c93ef1b56bed01df306d8b3605c1fd6b",
+    "url": "/science/static/media/35.c93ef1b5.png"
+  },
+  {
+    "revision": "db89badbfddcf8577279fe0e96b05af1",
+    "url": "/science/static/media/37.db89badb.png"
+  },
+  {
+    "revision": "403037b04de2965f0a47491d71933f28",
+    "url": "/science/static/media/38.403037b0.png"
+  },
+  {
+    "revision": "20abbea81897230e7b985a221f810e98",
+    "url": "/science/static/media/39.20abbea8.png"
+  },
+  {
+    "revision": "560c5f81cbe99a953264d06e0c7cd014",
+    "url": "/science/static/media/4.560c5f81.png"
+  },
+  {
+    "revision": "ccc5d2dd89f42b3c8991eca39bdd7986",
+    "url": "/science/static/media/40.ccc5d2dd.png"
+  },
+  {
+    "revision": "eea42071967c990ec47110017f77e4d4",
+    "url": "/science/static/media/41.eea42071.png"
+  },
+  {
+    "revision": "e26582ecb2a2fe9774bf34107aed71e1",
+    "url": "/science/static/media/42.e26582ec.png"
+  },
+  {
+    "revision": "ff36c683fba79d9568edaf2d714cb066",
+    "url": "/science/static/media/43.ff36c683.png"
+  },
+  {
+    "revision": "40b97ecd399aaa848af14e70689a2bed",
+    "url": "/science/static/media/44.40b97ecd.png"
+  },
+  {
+    "revision": "eacd1490e0424542571b019369a01fe9",
+    "url": "/science/static/media/45.eacd1490.png"
+  },
+  {
+    "revision": "ad6cf83e9ded1abe81fb365462c8f4d7",
+    "url": "/science/static/media/46.ad6cf83e.png"
+  },
+  {
+    "revision": "6f04579f42ad580f201faf4ad35b0c81",
+    "url": "/science/static/media/47.6f04579f.png"
+  },
+  {
+    "revision": "7498fd0032f151294c930c93367bd0f9",
+    "url": "/science/static/media/48.7498fd00.png"
+  },
+  {
+    "revision": "3fda3c9da4ec590c5c88e647022a3170",
+    "url": "/science/static/media/49.3fda3c9d.png"
+  },
+  {
+    "revision": "8506c659726847019116c0159642b02c",
+    "url": "/science/static/media/5.8506c659.png"
+  },
+  {
+    "revision": "612985c4d71cd2ef03008c8eaca76565",
+    "url": "/science/static/media/50.612985c4.png"
+  },
+  {
+    "revision": "15ef5fbda60a9f2559b901ba1fae4485",
+    "url": "/science/static/media/51.15ef5fbd.png"
+  },
+  {
+    "revision": "9f699579df78dbbfbf2a4e1bff8b2ce0",
+    "url": "/science/static/media/52.9f699579.png"
+  },
+  {
+    "revision": "5958b1ecfa6c8a21320db07c4c44221a",
+    "url": "/science/static/media/53.5958b1ec.png"
+  },
+  {
+    "revision": "47df90f219856aa11addc7a1fdce1ea2",
+    "url": "/science/static/media/54.47df90f2.png"
+  },
+  {
+    "revision": "232132e841d3930cf9312824e87e8285",
+    "url": "/science/static/media/55.232132e8.png"
+  },
+  {
+    "revision": "6f40b925808bcfceef28ee492480e926",
+    "url": "/science/static/media/56.6f40b925.png"
+  },
+  {
+    "revision": "a989d2c605289d1ad02c3ab3f3dd0981",
+    "url": "/science/static/media/57.a989d2c6.png"
+  },
+  {
+    "revision": "48d2eef01cecd55520ff1bf10c3e3748",
+    "url": "/science/static/media/58.48d2eef0.gif"
+  },
+  {
+    "revision": "212c4bcb95a3ce810e745bff36eb4bf4",
+    "url": "/science/static/media/59.212c4bcb.gif"
+  },
+  {
+    "revision": "e036ffe49c3290b294f2edb25b336a5d",
+    "url": "/science/static/media/6.e036ffe4.png"
+  },
+  {
+    "revision": "d9e398530b7031d5833f171d8888fc56",
+    "url": "/science/static/media/60.d9e39853.png"
+  },
+  {
+    "revision": "ca4158825f66b0587acca1862b795eaf",
+    "url": "/science/static/media/61.ca415882.png"
+  },
+  {
+    "revision": "5d448f98f7dd2c51dd322bd52424ed03",
+    "url": "/science/static/media/62.5d448f98.gif"
+  },
+  {
+    "revision": "efef2d82cf68e13a2f73dc6733505d08",
+    "url": "/science/static/media/63.efef2d82.gif"
+  },
+  {
+    "revision": "9453a215e8569bc0d48dd800f29684ba",
+    "url": "/science/static/media/64.9453a215.png"
+  },
+  {
+    "revision": "722a23b1191d46966aedd78d95fca9d8",
+    "url": "/science/static/media/65.722a23b1.png"
+  },
+  {
+    "revision": "3a59e185a90898933e52f25349ae4d7d",
+    "url": "/science/static/media/66.3a59e185.png"
+  },
+  {
+    "revision": "a73954d12550782b823ef827c6d3cc04",
+    "url": "/science/static/media/67.a73954d1.png"
+  },
+  {
+    "revision": "e0e17d693f0783d24c29a0c8383fc411",
+    "url": "/science/static/media/68.e0e17d69.png"
+  },
+  {
+    "revision": "d08c52463318dd0ee1dffa8b658fef8d",
+    "url": "/science/static/media/69.d08c5246.png"
+  },
+  {
+    "revision": "18ba7a065467efa32ebef20844f445f9",
+    "url": "/science/static/media/7.18ba7a06.png"
+  },
+  {
+    "revision": "2c2e10d5e4597575013bd5388af7662b",
+    "url": "/science/static/media/70.2c2e10d5.png"
+  },
+  {
+    "revision": "1b7afdee160f75bf8b758c1b100e6820",
+    "url": "/science/static/media/71.1b7afdee.png"
+  },
+  {
+    "revision": "0b76266b164e54359c50f92b5c193615",
+    "url": "/science/static/media/72.0b76266b.png"
+  },
+  {
+    "revision": "a498b93405f8a420b8596e3042875b7a",
+    "url": "/science/static/media/73.a498b934.png"
+  },
+  {
+    "revision": "158390bd9544af134029b848ffdba437",
+    "url": "/science/static/media/74.158390bd.png"
+  },
+  {
+    "revision": "d9e3697f4d0a19d5da13d82e6e7f01dc",
+    "url": "/science/static/media/75.d9e3697f.png"
+  },
+  {
+    "revision": "fa2009ed052a7d93fe4f547257da9d8d",
+    "url": "/science/static/media/76.fa2009ed.png"
+  },
+  {
+    "revision": "98f13a29f645b81034e5e646f9df1bb7",
+    "url": "/science/static/media/77.98f13a29.png"
+  },
+  {
+    "revision": "857ede3cc81b892dbd9b5529b67c2aae",
+    "url": "/science/static/media/78.857ede3c.png"
+  },
+  {
+    "revision": "c4d7394fbb6a92a56a1a91a02c1954e1",
+    "url": "/science/static/media/79.c4d7394f.png"
+  },
+  {
+    "revision": "6fc126f248e4b520c28a6229ca641bc0",
+    "url": "/science/static/media/8.6fc126f2.png"
+  },
+  {
+    "revision": "66cb2a611ff6d9cdb18b91b2fb8f5aa4",
+    "url": "/science/static/media/80.66cb2a61.png"
+  },
+  {
+    "revision": "d80fc2164d2bd6e69691e405df59d405",
+    "url": "/science/static/media/81.d80fc216.png"
+  },
+  {
+    "revision": "f252bea222e9691424554920f81f1e8a",
+    "url": "/science/static/media/82.f252bea2.png"
+  },
+  {
+    "revision": "c52550f12d3e398acfede775d4201c76",
+    "url": "/science/static/media/83.c52550f1.png"
+  },
+  {
+    "revision": "2e8a3d4d2880a5a93c3c5594e00fbc95",
+    "url": "/science/static/media/84.2e8a3d4d.png"
+  },
+  {
+    "revision": "74dc48ad95fb77a8d9a47fdd0b8fe85b",
+    "url": "/science/static/media/85.74dc48ad.gif"
+  },
+  {
+    "revision": "74dc48ad95fb77a8d9a47fdd0b8fe85b",
+    "url": "/science/static/media/86.74dc48ad.gif"
+  },
+  {
+    "revision": "46b6d158e19e9e163beaef495fa7c1b2",
+    "url": "/science/static/media/87.46b6d158.png"
+  },
+  {
+    "revision": "6983329e13ae9e92ff6fcebdc3f87db2",
+    "url": "/science/static/media/88.6983329e.svg"
+  },
+  {
+    "revision": "d43b471fb6ae005f637b915c0871f9e6",
+    "url": "/science/static/media/89.d43b471f.svg"
+  },
+  {
+    "revision": "091d77928921a2ccefc1456f02198651",
+    "url": "/science/static/media/9.091d7792.png"
+  },
+  {
+    "revision": "cb3620230c8f0e22ecf2d86b6464d1f2",
+    "url": "/science/static/media/90.cb362023.png"
+  },
+  {
+    "revision": "6c557182c62987ee0c4d894f29b444c0",
+    "url": "/science/static/media/91.6c557182.png"
+  },
+  {
+    "revision": "fa3b13a6617d74ed21e9e869a51d8f89",
+    "url": "/science/static/media/92.fa3b13a6.png"
+  },
+  {
+    "revision": "05c31e9fe88ee7c4281b6b90172c7ce5",
+    "url": "/science/static/media/93.05c31e9f.png"
+  },
+  {
+    "revision": "0c532cb62ab30c39f17c81bf7e35ea89",
+    "url": "/science/static/media/bgnow-01.0c532cb6.jpg"
+  },
+  {
+    "revision": "efc0e3314bf0a9623a140aa5e147db9f",
+    "url": "/science/static/media/1.efc0e331.gif"
+  },
+  {
+    "revision": "1b13f75ceb350717cc76c533f8b0aaab",
+    "url": "/science/static/media/10.1b13f75c.svg"
+  },
+  {
+    "revision": "cbca67ea7ec8bc6d0c162c7824a18ae9",
+    "url": "/science/static/media/11.cbca67ea.svg"
+  },
+  {
+    "revision": "9c8d37d365930b2cc972299abf820949",
+    "url": "/science/static/media/12.9c8d37d3.svg"
+  },
+  {
+    "revision": "bd266246b4a0532f63c4cd7ec7f3edb2",
+    "url": "/science/static/media/13.bd266246.svg"
+  },
+  {
+    "revision": "dff99a3586ce95c17f7f88648c1b366b",
+    "url": "/science/static/media/14.dff99a35.svg"
+  },
+  {
+    "revision": "39500ad467ffd39b4544dd5bb0354f54",
+    "url": "/science/static/media/15.39500ad4.svg"
+  },
+  {
+    "revision": "0c00ed2e134c5e12195104d00496abc2",
+    "url": "/science/static/media/16.0c00ed2e.svg"
+  },
+  {
+    "revision": "8749623fb3fffe051ec2ab2c799e45ea",
+    "url": "/science/static/media/17.8749623f.svg"
+  },
+  {
+    "revision": "4e5f5394a029d038eb6a971a3043b63e",
+    "url": "/science/static/media/18.4e5f5394.svg"
+  },
+  {
+    "revision": "be9ac776b0a2d7b1af887ee8c0e5ec9e",
+    "url": "/science/static/media/19.be9ac776.svg"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/science/static/media/2.b512cd28.gif"
+  },
+  {
+    "revision": "a69688b7ef0c8d7e50b301003edb17c9",
+    "url": "/science/static/media/20.a69688b7.svg"
+  },
+  {
+    "revision": "d8130a704441cb5bb00ba50ea5fd01e2",
+    "url": "/science/static/media/21.d8130a70.svg"
+  },
+  {
+    "revision": "5b798e09de4ea6adeff6dbe268949a43",
+    "url": "/science/static/media/22.5b798e09.svg"
+  },
+  {
+    "revision": "1f49dcd431c5ed10c7b1ffd7969824f6",
+    "url": "/science/static/media/23.1f49dcd4.svg"
+  },
+  {
+    "revision": "c5c43714054a40b714a2a380c55480e2",
+    "url": "/science/static/media/24.c5c43714.svg"
+  },
+  {
+    "revision": "30400720d86656248ae3ee498fddb683",
+    "url": "/science/static/media/25.30400720.svg"
+  },
+  {
+    "revision": "74cd2dffcec04bae28ccb59440a731f0",
+    "url": "/science/static/media/26.74cd2dff.svg"
+  },
+  {
+    "revision": "4684aa98f8fb82bc26fed9df5ebe237e",
+    "url": "/science/static/media/27.4684aa98.svg"
+  },
+  {
+    "revision": "6d7791f9d777ba33f81d743d5b1a9474",
+    "url": "/science/static/media/28.6d7791f9.svg"
+  },
+  {
+    "revision": "f5ae5e0741d62f79274d0962b5f23071",
+    "url": "/science/static/media/29.f5ae5e07.svg"
+  },
+  {
+    "revision": "5f1c11259cb15bc3800a9bb43a4a7242",
+    "url": "/science/static/media/3.5f1c1125.gif"
+  },
+  {
+    "revision": "821cde131dca119a05d6f45ed5115aad",
+    "url": "/science/static/media/4.821cde13.gif"
+  },
+  {
+    "revision": "af18e021df0acbf0ed1934fd8437f500",
+    "url": "/science/static/media/5.af18e021.jpg"
+  },
+  {
+    "revision": "feb5004056ff55a865af539beac0c510",
+    "url": "/science/static/media/6.feb50040.gif"
+  },
+  {
+    "revision": "c7b5bd2ec76796b984ce1a44875f110a",
+    "url": "/science/static/media/7.c7b5bd2e.png"
+  },
+  {
+    "revision": "069c3dd5d9df6d96884760a0bf18eec8",
+    "url": "/science/static/media/8.069c3dd5.svg"
+  },
+  {
+    "revision": "1b13f75ceb350717cc76c533f8b0aaab",
+    "url": "/science/static/media/9.1b13f75c.svg"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/science/static/media/1.a0d6b138.mp3"
+  },
+  {
+    "revision": "9cc63164328c5556a078f6a2cdf7fcab",
+    "url": "/science/static/media/2.9cc63164.mp3"
+  },
+  {
+    "revision": "512df663b03f9a647d7f9ba002d6b6fd",
+    "url": "/science/static/media/3.512df663.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/science/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "6d3e6e31968de6abd43c75375d97964a",
+    "url": "/science/static/media/Correct-answer.6d3e6e31.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/science/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/science/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/science/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "b8fca396c389eea881872cc7f0e7855b",
+    "url": "/science/static/media/321go.b8fca396.mp3"
+  },
+  {
+    "revision": "88e8a851b1954501951ae45cf46c1e2f",
+    "url": "/science/static/media/rightAnswer.88e8a851.mp3"
+  },
+  {
+    "revision": "1e695820a1572347f6a0a76f53280775",
+    "url": "/science/static/media/tostartpressbutton.1e695820.mp3"
+  },
+  {
+    "revision": "48b32efb6257ce0ee9f40b87b5894871",
+    "url": "/science/static/media/wrongAnswer.48b32efb.mp3"
+  },
+  {
+    "revision": "4dd7bd10b35bf639e1971d9f5d87b3b8",
+    "url": "/science/index.html"
+  }
+];
